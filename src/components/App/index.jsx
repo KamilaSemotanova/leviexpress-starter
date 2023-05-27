6e94abaf4e159578a2e87f4b7eef09ef6f885ef7
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-05-27 10:37:11 +0200

    router added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+  Outlet,
+} from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { JourneyPicker } from '../JourneyPicker';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Outlet />
     <Footer />
   </>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import './style.css';
+
+export const Reservation = () => {
+  return (
+    <div>
+      <h2>Detail jízdenky</h2>
+    </div>
+  );
+};
+
+
